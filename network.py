e64b1445ab509514ea37b272461c977523275972
Taehoon Kim <carpedm20@gmail.com>
2016-12-30 11:06:54 +0900

    loss and optim in progress

--- network.py
+++ network.py
@@ -1,55 +1,74 @@
 import tensorflow as tf
 from tensorflow.contrib.framework import arg_scope
+import tf.nn.sigmoid_cross_entropy_with_logits as CE_loss
 
 from layers import *
 
+def to_float(layer):
+  return tf.image.convert_image_dtype(layer, tf.float32)
+
 class Network(object):
   def __init__(self, config):
+    self.lambda_ = config.lambda_
+
     input_dims = [
         None, config.input_height,
         config.input_width, config.input_channel,
     ]
 
-    def to_float(layer):
-      return tf.image.convert_image_dtype(layer, tf.float32)
-
-    self.inputs = to_float(tf.placeholder(tf.uint8, input_dims, 'inputs'))
-    #self.input_real = to_float(tf.placeholder(tf.uint8, input_dims, 'input_real'))
-    #self.input_synthetic = to_float(tf.placeholder(tf.uint8, input_dims, 'input_synthetic'))
+    self.x = to_float(tf.placeholder(tf.uint8, input_dims, 'x'))
     self.targets = to_float(tf.placeholder(tf.uint8, input_dims, 'targets'))
 
     self.layer_dict = {}
 
     with arg_scope([resnet_block, conv2d, max_pool2d], layer_dict=self.layer_dict):
-      self.refiner_outputs = self._build_refiner_network()
+      self.R_x = self._build_refiner_network()
 
-      self.discrim_inputs = self._build_discriminator_network(self.inputs)
-      self.discrim_refiner = self._build_discriminator_network(self.refiner_outputs)
-      import ipdb; ipdb.set_trace() 
+      self.D_x = self._build_discriminator_network(self.x)
+      self.D_R_x = self._build_discriminator_network(self.R_x)
 
       #self.estimate_outputs = self._build_estimation_network()
 
-    self.refiner_loss = tf.reduce_sum(self.refiner_outputs - self.inputs, [1, 2, 3])
+  def _build_loss(self):
+    # Refiner loss
+    real_label = tf.ones_like(self.D_R_x)
+    fake_label = tf.zeros_like(self.D_R_x)
+
+    with tf.variable_scope("refiner"):
+      realism_loss = CE_loss(self.D_R_x, real_label)
+      regularization_loss = \
+          self.lambda_ * tf.reduce_sum(self.R_x - self.x, [1, 2, 3])
+
+    # Discriminator loss
+    with tf.variable_scope("discriminator"):
+      refiner_d_loss = CE_loss(self.D_R_x, fake_label)
+      synthetic_d_loss = CE_loss(self.D_x, real_label)
+
+      self.discrim_loss = tf.reduce_mean(
+          self.refiner_d_loss + self.synthetic_d_loss)
 
   def _build_refiner_network(self):
-    layer = self.inputs
-    with tf.variable_scope("refiner"):
+    layer = self.x
+    with tf.variable_scope("refiner") as sc:
       layer = repeat(layer, 5, resnet_block, scope="resnet")
       layer = conv2d(layer, 1, 1, 1, scope="conv_1")
+      self.refiner_variables = sc.get_variable()
     return layer
 
   def _build_discriminator_network(self, layer):
-    with tf.variable_scope("discriminator"):
+    with tf.variable_scope("discriminator") as sc:
       layer = conv2d(layer, 96, 3, 2, scope="conv_1")
       layer = conv2d(layer, 64, 3, 2, scope="conv_2")
       layer = max_pool2d(layer, 3, 1, scope="max_1")
       layer = conv2d(layer, 32, 3, 1, scope="conv_3")
       layer = conv2d(layer, 32, 1, 1, scope="conv_4")
-      layer = conv2d(layer, 2, 1, 1, activation_fn=tf.nn.softmax, scope="conv_5")
+      layer = conv2d(layer, 2, 1, 1, 
+          activation_fn=tf.nn.softmax, scope="conv_5")
+      self.discriminator_variables = sc.get_variable()
     return layer
 
   def _build_estimation_network(self):
-    layer = self.inputs
+    layer = self.x
     with tf.variable_scope("estimation"):
       layer = conv2d(layer, 96, 3, 2, scope="conv_1")
       layer = conv2d(layer, 64, 3, 2, scope="conv_2")
--- trainer.py
+++ trainer.py
@@ -5,14 +5,28 @@ from tensorflow.contrib.framework.python.ops import arg_scope
 
 from network import Network
 
-class Model(object):
+class Trainer(object):
   def __init__(self, config):
     self.K_d = config.K_d
     self.K_g = config.K_g
 
+    self.initial_K_d = config.initial_K_d
+    self.initial_K_g = config.initial_K_g
+
+    self.learning_rate = config.learning_rate
+
     self.network = Network(config)
 
+  def build_optim(self):
+    self.optim = tf.train.GradientDescentOptimizer(self.learning_rate)
+
   def train(self):
+    for k in range(self.initial_K_g):
+      pass
+
+    for k in range(self.initial_K_d):
+      pass
+
     for step in range(self.max_step):
       for k in range(self.K_g):
         pass
